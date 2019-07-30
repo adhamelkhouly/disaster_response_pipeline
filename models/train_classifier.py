9864c2dfffdc2116d9091b222ce1d24b589fa354
Adham El-Khouly <adham.elkhouly@hotmail.com>
2019-07-30 04:52:57 -0400

    EDIT pipleline to one that worked better Went through numerous iterations of picking a ML algorithm that works best for the dataset provided and this pipeline provided an average f1-score of 0.95

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -4,21 +4,17 @@ import pandas as pd
 import pickle
 from sqlalchemy import create_engine
 
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
-from sklearn.svm import SVC
-from sklearn.svm import LinearSVC
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.multioutput import MultiOutputClassifier
-from sklearn.multiclass import OneVsRestClassifier
 from sklearn.metrics import classification_report
-from sklearn.tree import DecisionTreeClassifier
-
 
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
+from nltk import FreqDist
 
 import nltk
 import warnings
@@ -29,6 +25,7 @@ warnings.filterwarnings('ignore')
 def load_data(database_filepath):
     """
     Loads a database into a pd.DataFrame and returns
+
     Args:
         database_filepath(str): file path to a .db database file
 
@@ -40,17 +37,24 @@ def load_data(database_filepath):
     y = df[df.columns[3:]]
     category_names = list(y.columns)
 
-    return X, y, category_names
+    # To be plotted later on, this extracts the most commonly used words
+    messages = df['message'].values.tolist()
+    text = ' '.join(messages)
+    text = tokenize(text)
+    dist = FreqDist(w for w in text)
+    most_common = dict(dist.most_common(10))
+
+    return X, y, category_names, most_common
 
 
 def tokenize(text):
     """
     Normalizes and tokenizes messages
+
     Args:
         text:
 
     Returns:
-
     """
     # Normalizing Text
     text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
@@ -74,22 +78,24 @@ def build_model():
 
     Returns: sklearn.pipeline.Pipeline
     """
+    # build pipeline
     pipeline = Pipeline([
         ('vect', CountVectorizer(tokenizer=tokenize)),
         ('tfidf', TfidfTransformer()),
-        ('clf', OneVsRestClassifier(
-            LinearSVC()))
+        ('clf', MultiOutputClassifier(RandomForestClassifier())),
     ])
 
-    # Set parameters for gird search
+    # set parameters for grid search
     parameters = {
-        'clf__estimator__C': range(1, 11),
-        'clf__estimator__verbose': [3],
-        'clf__n_jobs': [4]
+        'vect__max_df': (0.5, 0.75, 1.0),
+        'vect__ngram_range': ((1, 1), (1, 2)),
+        'tfidf__use_idf': [True, False],
+        'tfidf__norm': ['l1', 'l2']
     }
 
     # Set grid search
-    cv = GridSearchCV(pipeline, param_grid=parameters, cv=2, verbose=3)
+    cv = GridSearchCV(pipeline, param_grid=parameters, cv=2,
+                      verbose=3, n_jobs=-1)
 
     return cv
 
@@ -99,30 +105,34 @@ def evaluate_model(model, X_test, y_test, category_names):
     Uses sklearn's classification report to evaluate the model
 
     Args:
-        model(sklearn.multioutput.MultiOutputClassifier): a pipeline or ml model
-        X_test(pd.Series): test data
-        y_test(pd.Series): target values test data
+        model(GridSearchCV): a pipeline or ml model
+        X_test(pd.DataFrame): test data
+        y_test(pd.DataFrame): target values test data
         category_names(list): column names for target values
 
     Returns: None
     """
     y_pred = model.predict(X_test)
-    print(classification_report(y_test.values, y_pred,
-                                target_names=category_names))
+    print('Best parameters for model are: ', model.best_params_)
+    for index, cat in enumerate(category_names):
+        print(cat)
+        print(classification_report(y_test.iloc[:, index], y_pred[:, index]))
 
 
-def save_model(model, model_filepath):
+def save_model(model, model_filepath, most_common_words):
     """
     Saves the model to a pickle file
 
     Args:
         model:
         model_filepath:
+        most_common_words:
 
     Returns:
 
     """
-    pickle.dump(model, open(model_filepath, 'wb'))
+    with open(model_filepath, 'wb') as f:
+        pickle.dump((model, most_common_words), f)
 
 
 def main():
@@ -135,9 +145,9 @@ def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, y, category_names = load_data(database_filepath)
+        X, y, category_names, most_common = load_data(database_filepath)
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-        
+        print(y.sum())
         print('Building model...')
         model = build_model()
         
@@ -148,7 +158,7 @@ def main():
         evaluate_model(model, X_test, y_test, category_names)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
-        save_model(model, model_filepath)
+        save_model(model, model_filepath, most_common)
 
         print('Trained model saved!')
 
@@ -156,7 +166,7 @@ def main():
         print('Please provide the filepath of the disaster messages database '
               'as the first argument and the filepath of the pickle file to '
               'save the model to as the second argument. \n\nExample: python '
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+              'train_classifier.py ../data/DisasterResponse.db model.pkl')
 
 
 if __name__ == '__main__':
