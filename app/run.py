c6dc9745d812bc78147126218ccacdf5dd11f470
Adham El-Khouly <adham.elkhouly@hotmail.com>
2019-07-29 22:21:34 -0400

    ADD plot1, plot2, and plot3 functions plot1: simple function for plotting the categorical count plot2: plots top words used plot3: plots genre counts

--- app/run.py
+++ app/run.py
@@ -1,9 +1,13 @@
 import json
 import plotly
 import pandas as pd
+import re
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk import FreqDist
+
 
 from flask import Flask
 from flask import render_template, request, jsonify
@@ -16,24 +20,149 @@ app = Flask(__name__)
 
 
 def tokenize(text):
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
+    """
+    Normalizes and tokenizes messages
+    Args:
+        text:
 
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
+    Returns:
+
+    """
+    # Normalizing Text
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+
+    # Tokenizing Text
+    words = word_tokenize(text)
+
+    # Remove Stop words
+    words = [w for w in words if w not in stopwords.words("english")]
+
+    # Lemmatize
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = [lemmatizer.lemmatize(x).strip() for x in words]
 
     return clean_tokens
 
 
+def plot1(df):
+    """
+    Graph 1 of the categorical count
+
+    Args:
+        df (pd.DataFrame): loaded DataFrame from database file
+
+    Returns: dict
+    """
+    categories_count = df[df.columns[4:]].sum(axis=0)
+    graph = {
+        'data': [
+            go.Bar(
+                x=df.columns[3:],
+                y=categories_count
+            )
+        ],
+
+        'layout': {
+            'title': 'Distribution of Message Categories',
+            'yaxis': {
+                'title': "Count"
+            },
+            'xaxis': {
+                'title': "Category"
+            }
+        }
+    }
+    return graph
+
+
+def plot2(df):
+    """
+    Graph 2 of the top words used
+
+    Args:
+        df (pd.DataFrame): loaded DataFrame from database file
+
+    Returns: dict
+    """
+    messages = df['message'].values.tolist()
+    text = ' '.join(messages)
+    text = tokenize(text)
+    dist = FreqDist(w for w in text)
+    most_common = dict(dist.most_common(10))
+
+    graph = {
+        'data': [
+            go.Bar(
+                x=most_common.keys(),
+                y=most_common.values()
+            )
+        ],
+
+        'layout': {
+            'title': 'Distribution of Top Words Used',
+            'yaxis': {
+                'title': "Count"
+            },
+            'xaxis': {
+                'title': "Word"
+            }
+        }
+    }
+    return graph
+
+
+def plot3(df):
+    """
+    Graph 3 of the message genres
+
+    Args:
+        df (pd.DataFrame): loaded DataFrame from database file
+
+    Returns: dict
+    """
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+
+    graph = {
+            'data': [
+                go.Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        }
+
+    return graph
+
+
+def plot4(df):
+    """
+    Graph 4 of categories per genre
+
+    Args:
+        df (pd.DataFrame): loaded DataFrame from database file
+
+    Returns: dict
+    """
+    pass
+
+
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('response', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
-
+model = joblib.load("../models/classifier.pkl")
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
